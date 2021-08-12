876e3d0c300ae133ddd6c526b08517cea6a1e0ab
Thiago May <thiago.may1@gmail.com>
2021-08-12 00:11:42 -0300

    Fix serializers in company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,6 @@
 class Company < ApplicationRecord
-  include ActiveModel::Serialization
 
   has_many :employees
   validates :name, presence: true, uniqueness: true
 
-  def attributes
-    { :id => id, :name => name }
-  end
 end
